5018dd9f0658289476bb2fe43ce3ae712ebce576
Uday Kumar Chintala <udaych20@gmail.com>
2023-03-20 12:05:12 +0530

    S3 Source 1970 & 1971 Issue Code Changes Internal Review
    Signed-off-by: Uday Kumar Chintala <udaych20@gmail.com>

---
+++ data-prepper-plugins/s3-source/src/main/java/org/opensearch/dataprepper/plugins/source/S3ObjectHandler.java
@@ -0,0 +1,11 @@
+package org.opensearch.dataprepper.plugins.source;
+
+import java.io.IOException;
+
+/**
+ * A S3ObjectHandler interface must be extended/implement for S3 Object parsing
+ *
+ */
+public interface S3ObjectHandler {
+    void parseS3Object(final S3ObjectReference s3ObjectReference) throws IOException;
+}
